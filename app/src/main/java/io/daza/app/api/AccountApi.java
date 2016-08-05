063c3c4cd2a6dbba7a82e660c077d881f8830a30
Lijy91 <lijy91@foxmail.com>
2016-08-05 19:10:46 +0800

    model

--- app/src/main/java/io/daza/app/api/AccountApi.java
+++ app/src/main/java/io/daza/app/api/AccountApi.java
@@ -1,8 +1,27 @@
 package io.daza.app.api;
 
+import io.daza.app.model.Result;
+import io.daza.app.model.User;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface AccountApi {
 
-    void register();
-    void login();
-    void logout();
+    @FormUrlEncoded
+    @POST("account/register")
+    Call<Result<User>> register(@Field("username") String username,
+                                @Field("email") String email,
+                                @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("account/login")
+    Call<Result<User>> login(@Field("email") String email,
+                             @Field("password") String password);
+
+    @FormUrlEncoded
+    @POST("account/logout")
+    Call<Result> logout();
+
 }
