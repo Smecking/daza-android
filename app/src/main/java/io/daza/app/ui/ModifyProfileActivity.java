bfa0d25e17dbc9947bf94863da75b602cb1cbc57
Lijy91 <lijy91@foxmail.com>
2016-12-03 20:08:24 +0800

    修改资料页清空无用代码

--- app/src/main/java/io/daza/app/ui/ModifyProfileActivity.java
+++ app/src/main/java/io/daza/app/ui/ModifyProfileActivity.java
@@ -17,11 +17,6 @@
 package io.daza.app.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import org.blankapp.app.BaseActivity;
 
@@ -33,17 +28,7 @@ public class ModifyProfileActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
