399b42c41e9d614f65bad5703adcda5ffd2754c2
Lijy91 <lijy91@foxmail.com>
2016-12-04 22:41:44 +0800

    账户与安全页面设置

--- app/src/main/java/io/daza/app/ui/AccountActivity.java
+++ app/src/main/java/io/daza/app/ui/AccountActivity.java
@@ -16,16 +16,51 @@
 
 package io.daza.app.ui;
 
+import android.content.Intent;
+import android.support.v14.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import io.daza.app.R;
+import io.daza.app.ui.base.BaseActivity;
+import io.daza.app.util.Auth;
 
-public class AccountActivity extends AppCompatActivity {
+public class AccountActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+    }
+
+
+    public static class AccountFragment extends PreferenceFragment {
+
+        private static final String KEY_EMAIL = "key_email";
+        private static final String KEY_MODIFY_PASSWORD = "key_modify_password";
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            addPreferencesFromResource(R.xml.pref_account);
+
+            if (Auth.check()) {
+                findPreference(KEY_EMAIL).setSummary(Auth.user().getEmail());
+            } else {
+                findPreference(KEY_EMAIL).setSummary("");
+            }
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(Preference preference) {
+            if (KEY_MODIFY_PASSWORD.equals(preference.getKey())) {
+                Intent intent = new Intent(getActivity(), ModifyPasswordActivity.class);
+                startActivity(intent);
+            }
+            return true;
+        }
     }
 }
