2ea424827b538d47ce696cc670d4b7be93a29a88
Lijy91 <lijy91@foxmail.com>
2016-12-05 23:36:12 +0800

    修改资料页面

--- app/src/main/java/io/daza/app/ui/ModifyProfileActivity.java
+++ app/src/main/java/io/daza/app/ui/ModifyProfileActivity.java
@@ -17,18 +17,81 @@
 package io.daza.app.ui;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
-import org.blankapp.app.BaseActivity;
+import org.blankapp.annotation.ViewById;
+import org.blankapp.validation.Rule;
+import org.blankapp.validation.Validator;
 
 import io.daza.app.R;
+import io.daza.app.model.ArticleComment;
+import io.daza.app.model.Result;
+import io.daza.app.model.User;
+import io.daza.app.ui.base.BaseActivity;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static io.daza.app.api.ApiClient.API;
 
 public class ModifyProfileActivity extends BaseActivity {
+
+    @ViewById(R.id.edt_name)
+    private EditText mEdtName;
+    @ViewById(R.id.edt_city)
+    private EditText mEdtCity;
+    @ViewById(R.id.edt_website)
+    private EditText mEdtWebsite;
+    @ViewById(R.id.edt_bio)
+    private EditText mEdtBio;
+
+    private Validator mValidator = new Validator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_profile);
 
+        mValidator.add(Rule.with(mEdtName).required());
+        mValidator.add(Rule.with(mEdtCity).required());
+        mValidator.add(Rule.with(mEdtBio).required());
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_modify_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_send) {
+            if (!mValidator.validate()) {
+                return false;
+            }
+            String name = mEdtName.getText().toString();
+            String city = mEdtCity.getText().toString();
+            String website = mEdtWebsite.getText().toString();
+            String bio = mEdtBio.getText().toString();
+            API.updateProfile(name, city, website, bio).enqueue(new Callback<Result<User>>() {
+                @Override
+                public void onResponse(Call<Result<User>> call, Response<Result<User>> response) {
+                    if (response.isSuccessful()) {
+                        finish();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<Result<User>> call, Throwable t) {
+
+                }
+            });
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
