8bd671c7c2a2b7f0b0472bfce36c05dc218dbd10
Lijy91 <lijy91@foxmail.com>
2016-12-05 17:32:28 +0800

    拦截所有非daza.io域名的url邮系统浏览器打开

--- app/src/main/java/io/daza/app/ui/InAppBrowserActivity.java
+++ app/src/main/java/io/daza/app/ui/InAppBrowserActivity.java
@@ -16,17 +16,141 @@
 
 package io.daza.app.ui;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.net.http.SslError;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
+import org.blankapp.util.ViewUtils;
+
+import java.util.Map;
+
+import io.daza.app.BuildConfig;
 import io.daza.app.R;
+import io.daza.app.ui.base.BaseActivity;
 import io.daza.app.ui.base.BaseWebViewActivity;
 
-public class InAppBrowserActivity extends BaseWebViewActivity {
+public class InAppBrowserActivity extends BaseActivity {
+
+    protected WebView mWebView;
+    protected ProgressBar mProgressBar;
+
+    public WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (url.contains("http://") || url.contains("https://")) {
+                if (!url.contains(BuildConfig.DOMAIN_NAME)) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            InAppBrowserActivity.this.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            InAppBrowserActivity.this.onPageFinished(view, url);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            InAppBrowserActivity.this.onReceivedError(view, errorCode, description, failingUrl);
+        }
+
+        @Override
+        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+            handler.proceed();
+        }
+    };
+
+    public WebChromeClient mWebChromeClient = new WebChromeClient() {
+        public void onProgressChanged(WebView view, int newProgress) {
+            InAppBrowserActivity.this.onProgressChanged(view, newProgress);
+        }
+
+        @Override
+        public void onReceivedTitle(WebView view, String title) {
+            InAppBrowserActivity.this.onReceivedTitle(view, title);
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_app_browser);
+    }
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        mWebView = (WebView) findViewById(R.id.webview);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressbar);
+        mWebView.setWebViewClient(mWebViewClient);
+        mWebView.setWebChromeClient(mWebChromeClient);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void loadUrl(java.lang.String url, Map<String, String> additionalHttpHeaders) {
+        mWebView.loadUrl(url, additionalHttpHeaders);
+    }
+
+    public void loadUrl(java.lang.String url) {
+        mWebView.loadUrl(url);
+    }
+
+    public void onPageStarted(WebView view, String url, Bitmap favicon) {
+    }
+
+    public void onPageFinished(WebView view, String url) {
+    }
+
+    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+    }
+
+    public void onProgressChanged(WebView view, int newProgress) {
+        if (mProgressBar != null) {
+            mProgressBar.setMax(100);
+            mProgressBar.setProgress(newProgress);
+            // 如果进度大于或者等于100，则隐藏进度条
+            ViewUtils.setGone(mProgressBar, newProgress >= 100);
+        }
+    }
+
+    public void onReceivedTitle(WebView view, String title) {
+        setTitle(title);
     }
 }
