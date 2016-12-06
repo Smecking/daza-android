139fa3f507d5dbfc22bdbc9cdbaee38290ed2edd
Lijy91 <lijy91@foxmail.com>
2016-12-06 16:09:16 +0800

    加入空指示，和错误指示

--- app/src/main/java/io/daza/app/ui/base/BaseListFragment.java
+++ app/src/main/java/io/daza/app/ui/base/BaseListFragment.java
@@ -20,16 +20,30 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.text.TextUtils;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.blankapp.app.ListFragment;
+import org.blankapp.util.ViewUtils;
 
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import io.daza.app.R;
 import io.daza.app.model.Pagination;
 
 public abstract class BaseListFragment<VH extends RecyclerView.ViewHolder, Item, Result> extends
         ListFragment<VH, Item, Result> {
+
+    private ProgressBar mPbLoading;
+    private TextView mTvLoading;
+    private TextView mTvEmpty;
+    private TextView mTvError;
+    private Button mBtnRetry;
 
     private Pagination mPagination;
     private boolean mIsLoadMore = false;
@@ -49,7 +63,6 @@ public abstract class BaseListFragment<VH extends RecyclerView.ViewHolder, Item,
 
                 @Override
                 public void onLoadMore(int page, int totalItemsCount) {
-                    Log.d("BaseListFragment", "" + page);
                     BaseListFragment.this.onLoadMore(page, totalItemsCount);
                 }
             };
@@ -70,26 +83,50 @@ public abstract class BaseListFragment<VH extends RecyclerView.ViewHolder, Item,
     @Override
     public void onLoadStart() {
         if (isEmpty()) {
-            // Do something
+            showLoading();
         }
         mEndlessRecyclerViewScrollListener.setLoading(true);
     }
 
     @Override
     public void onLoadError(Exception e) {
-        e.printStackTrace();
+        onRefreshComplete();
+        if (isEmpty()) {
+            String error = "";
+            if (e != null) {
+                e.printStackTrace();
+                //加入404与500
+                if (e instanceof UnknownHostException) {
+                    //无法解析---网络错误
+                    error = getString(R.string.network_not_connected);
+                } else if (e instanceof SocketTimeoutException) {
+                    error = getString(R.string.http_exception_error);
+                } else {
+                    error = e.getLocalizedMessage();
+                }
+            }
+            showError(error);
+        }
     }
 
     @Override
     public void onRefreshComplete() {
         super.onRefreshComplete();
         if (isEmpty()) {
-            // Do something
+            showEmpty();
         } else {
-            // Do something
+            hideEmptyIndicator();
         }
         mEndlessRecyclerViewScrollListener.setLoading(false);
         getAdapter().notifyDataSetChanged();
+    }
+
+    protected void firstRefresh() {
+        setPagination(null);
+        mEndlessRecyclerViewScrollListener.setLoading(true);
+        getItemsSource().clear();
+        this.onRefreshComplete();
+        this.onRefresh();
     }
 
     public void setPagination(Pagination pagination) {
@@ -101,5 +138,72 @@ public abstract class BaseListFragment<VH extends RecyclerView.ViewHolder, Item,
             return this.mPagination.getCurrent_page() + 1;
         }
         return 1;
+    }
+
+    public Item getItem(int position) {
+        return getItemsSource().get(position);
+    }
+
+    protected void showLoading() {
+        hideEmptyIndicator();
+        if (mPbLoading != null) ViewUtils.setVisible(mPbLoading, true);
+        if (mTvLoading != null) ViewUtils.setVisible(mTvLoading, true);
+    }
+
+    protected void showEmpty() {
+        hideEmptyIndicator();
+        if (mTvEmpty != null) ViewUtils.setVisible(mTvEmpty, true);
+    }
+
+    protected void showError(String error) {
+        hideEmptyIndicator();
+        if (mTvError != null) {
+            ViewUtils.setVisible(mTvError, true);
+            if (!TextUtils.isEmpty(error)) {
+                mTvError.setText(error);
+            } else {
+                mTvError.setText("an error occurred, please try again later");
+            }
+        }
+        if (mBtnRetry != null) {
+            ViewUtils.setVisible(mBtnRetry, true);
+            mBtnRetry.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onRefresh();
+                }
+            });
+        }
+    }
+
+    public void ensureEmptyIndicatorView() {
+        View view = getView();
+        if (view == null) {
+            return;
+        }
+        if (mPbLoading == null) {
+            mPbLoading = (ProgressBar) view.findViewById(R.id.pb_loading);
+        }
+        if (mTvLoading == null) {
+            mTvLoading = (TextView) view.findViewById(R.id.tv_loading);
+        }
+        if (mTvEmpty == null) {
+            mTvEmpty = (TextView) view.findViewById(R.id.tv_empty);
+        }
+        if (mTvError == null) {
+            mTvError = (TextView) view.findViewById(R.id.tv_error);
+        }
+        if (mBtnRetry == null) {
+            mBtnRetry = (Button) view.findViewById(R.id.btn_retry);
+        }
+    }
+
+    private void hideEmptyIndicator() {
+        ensureEmptyIndicatorView();
+        if (mPbLoading != null) ViewUtils.setGone(mPbLoading, true);
+        if (mTvLoading != null) ViewUtils.setGone(mTvLoading, true);
+        if (mTvEmpty != null) ViewUtils.setGone(mTvEmpty, true);
+        if (mTvError != null) ViewUtils.setGone(mTvError, true);
+        if (mBtnRetry != null) ViewUtils.setGone(mBtnRetry, true);
     }
 }
